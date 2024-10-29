bd833264142c6ad6a5aedc3ff902dd76dd0ca825
sgrupinski <stanislaw.grupinski@solve.md>
2024-10-29 13:42:01 +0100

    Created file anonymizer

--- csv_file_creator/create_file.py
+++ csv_file_creator/create_file.py
@@ -3,13 +3,13 @@ import csv
 from fake_person_generator import *
 
 
-def create_and_fill_csv_file():
+def create_and_fill_csv_file(records=20):
     # Creates file in a root directory and opens it in writing mode
     with open("../file_to_anonymize.csv", "w") as file:
         csv_file = csv.writer(file)
         # Creates first row of .csv file
         csv_file.writerow(["Name", "Surname", "PESEL", "Age"])
-        for x in range(100):
+        for x in range(records):
             name, surname, pesel, age = generate_person_information()
             csv_file.writerow([name, surname, pesel, age])
 
